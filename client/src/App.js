98ed14f0bf51b067ba49ec61603730e62f64c04d
Shaik Ameen <ameenshaik0077@gmail.com>
2023-10-04 11:12:34 +0530

    new changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>This is heading </h1>
-    <h2>This is Ameen's Shaik</h2>
-    <h1>This Is E-Commerce Project</h1>
-    <h1>Pushpa</h1>
-    <h1>how are you</h1>
-    <h1>tinnara</h1>
-    <h1>em chestunnaru</h1>
-    <h1>naku push and pull vachesindhile</h1>
-    <h1>inka deeni kosam sir daggariki vellakkarledhule</h1>
-    <h3>puspha fast ga ravali</h3>
-    <h1>hello</h1>
+    
     </div>
   );
 }
