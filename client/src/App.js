50990dbf3f11f0363706f7652ba060b8b2fc2521
BhavaniViyyapu <bhavaniviyyapu9@gmail.com>
2023-10-04 10:25:15 +0530

    modified src

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ function App() {
     <h1>em chestunnaru</h1>
     <h1>naku push and pull vachesindhile</h1>
     <h1>inka deeni kosam sir daggariki vellakkarledhule</h1>
+    <h1>How are you all!</h1>
     </div>
   );
 }
