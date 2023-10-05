5aa70001ec45f8fac8fb287be26c7206b0eec560
Shaik Ameen <ameenshaik0077@gmail.com>
2023-10-05 12:13:30 +0530

    new changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbaar from './components/header/Navbaar';
+import Newnav from './components/newnavbaar/Newnav';
+import Cart from './components/cart/Cart';
 
 function App() {
   return (
-    <div className="App">
-<<<<<<< HEAD
-    <h1>This is heading </h1>
-    <h2>This is Ameen's Shaik</h2>
-    <h1>This Is E-Commerce Project</h1>
-    <h1>Pushpa</h1>
-    <h1>how are you</h1>
-    <h1>tinnara</h1>
-    <h1>em chestunnaru</h1>
-    <h1>naku push and pull vachesindhile</h1>
-    <h1>inka deeni kosam sir daggariki vellakkarledhule</h1>
-    <h3>puspha fast ga ravali</h3>
-    <h1>hello</h1>
-=======
-
->>>>>>> 8ba4ef2915a7827475f01b7213b6068f6d1b6905
-    </div>
+  <>
+    <Navbaar/> 
+    <Newnav/>  
+    <Cart/>
+  </>
   );
 }
 
