72ee4a5c11f259e3dffd0e2d16b944e5d2b8f5cb
althaf shaik <althaf115110@gmail.com>
2023-10-06 15:10:45 +0530

    new changes

--- client/src/components/home/MainComp.js
+++ client/src/components/home/MainComp.js
@@ -18,6 +18,7 @@ function MainComp() {
             <Slide title="Deal of the day"/>
         </div>
 
+
         <div className='right_slide'>
             <h4>Festive latest launches</h4>
             <img src='https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Jupiter/Launches/T3/DesktopGateway_CategoryCard2x_758X608_T3._SY608_CB639883570_.jpg' alt="rightimg"></img>
